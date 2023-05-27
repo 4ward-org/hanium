84405599db850ee180a666a69cf769dfd7ccbadb
roy <86198214+dataKim1201@users.noreply.github.com>
2023-05-27 15:26:37 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,36 +1,57 @@
-# pip install -U torch transformers tokenizers accelerate
+from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
 import torch
-from transformers import pipeline, AutoModelForCausalLM
 
-def ask(x, context='', is_input_full=False):
-    ans = pipe(
-        f"### 질문: {x}\n\n### 맥락: {context}\n\n### 답변:" if context else f"### 질문: {x}\n\n### 답변:", 
-        do_sample=True, 
-        max_new_tokens=512,
-        temperature=0.7,
-        top_p=0.9,
-        return_full_text=False,
-        eos_token_id=2,
-    )
-    print(ans[0]['generated_text'])
 
-if __name__ =='__main__':
-    MODEL = 'beomi/KoAlpaca-Polyglot-5.8B'
+def ask(past_user_inputs=[],generated_responses=[]):
 
-    model = AutoModelForCausalLM.from_pretrained(
-        MODEL,
-        torch_dtype=torch.float16,
-        low_cpu_mem_usage=True,
-    ).to(device=f"cuda", non_blocking=True)
-    model.eval()
+    user_input = input(">> User:")
+    if user_input.strip() == 'q':
+        return 'q','',''
+    text_idx = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
+    for i in range(len(generated_responses)-1, len(generated_responses)-3, -1):
+        if i < 0:
+            break
+        encoded_vector = tokenizer.encode(generated_responses[i] + tokenizer.eos_token, return_tensors='pt')
+        if text_idx.shape[-1] + encoded_vector.shape[-1] < 1000:
+            text_idx = torch.cat([encoded_vector, text_idx], dim=-1)
+        else:
+            generated_responses.popleft()
+            
+            break
+        encoded_vector = tokenizer.encode(past_user_inputs[i] + tokenizer.eos_token, return_tensors='pt')
+        if text_idx.shape[-1] + encoded_vector.shape[-1] < 1000:
+            text_idx = torch.cat([encoded_vector, text_idx], dim=-1)
+        else:
+            past_user_inputs.popleft()
+            break
+    text_idx = text_idx.to(device)
+    inference_output = model.generate(
+            text_idx,
+            max_length=1000,
+            num_beams=5,
+            top_k=20,
+            no_repeat_ngram_size=4,
+            length_penalty=0.65,
+            repetition_penalty=1.0,
+        )
+    inference_output = inference_output.tolist()
+    bot_response = tokenizer.decode(inference_output[0][text_idx.shape[-1]:], skip_special_tokens=True)
+    past_user_inputs.append(user_input.strip())
+    generated_responses.append(bot_response)
+    return bot_response,past_user_inputs,generated_responses
+if __name__=='__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    pipe = pipeline(
-        'text-generation', 
-        model=model,
-        tokenizer=MODEL,
-        device=0
-    )
+    tokenizer = PreTrainedTokenizerFast.from_pretrained('byeongal/Ko-DialoGPT')
+    model = GPT2LMHeadModel.from_pretrained('byeongal/Ko-DialoGPT').to(device)
+    past_user_inputs = []
+    generated_responses = []
     while True:
-        ask_text = input('please ask something!')
-        if ask_text != 'q':
-            ask(ask_text)
+        bot_response,past_user_inputs,generated_responses = ask(past_user_inputs,generated_responses)
+        if bot_response =='q':
+            print('we are done here!')
+            break
+        else:
+            print(bot_response)
+            print('past_user_inputs',past_user_inputs)
+            print('generated_responses',generated_responses)
