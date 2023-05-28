cf0f301cb3e9866db0e0bdcdb19847294cbddc05
KHK <googija9818@gmail.com>
2023-05-28 19:29:43 +0900

    로그인 ajax 통신 연결
    *백이랑 로그인 연결을 위한 login.js 파일 생성

---
+++ app/src/login.js
@@ -0,0 +1,34 @@
+let userId = document.getElementById("userId");
+let pw = document.getElementById("password");
+let signBtn = document.getElementById("btn");
+
+function requestPostBodyJson() {
+  var reqURL = "http://127.0.0.1:8000/users/register/";
+  var jsonData = {
+    user_id: userId.value,
+    password: pw.value,
+  };
+  $.ajax({
+    url: "http://127.0.0.1:8000/users/register/",
+    data: JSON.stringify(jsonData),
+    type: "post",
+    async: false,
+    timeout: 5000,
+    dataType: "JSON",
+    contentType: "application/json; charset=utf-8",
+    // success: function (data, textStatus, xhr) {
+    //   if (data == "loginFail") {
+    //     alert("로그인에 실패하였습니다.");
+    //   } else {
+    //     window.location.href = "index.html";
+    //   }
+    // }
+  });
+}
+
+function test() {
+  console.log(userId.value);
+  console.log(pw.value);
+}
+
+signBtn.addEventListener("click", test);
